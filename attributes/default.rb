6409011f7e18500b8d543fd0fcffc5163f2c1aed
Mahmoud Ismail <mahmoud@logicalclocks.com>
2019-12-09 18:29:45 +0100

    fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -62,59 +62,59 @@ default['elastic']['exporter']['flags']         = %w[--es.all
 ]
 
 #OpenDistro Security Plugin
-default['elastic']['opendistro']['version'] = "1.2.0.0"
-default['elastic']['opendistro_security']['enabled'] = "true"
-default['elastic']['opendistro_security']['url'] = "#{node['download_url']}/opendistro_security-#{node['elastic']['opendistro']['version']}.zip"
-default['elastic']['opendistro_security']['base_dir'] = "#{node['elastic']['plugins_dir']}/opendistro_security"
-default['elastic']['opendistro_security']['config_dir'] = "#{node['elastic']['opendistro_security']['base_dir']}/securityconfig"
-default['elastic']['opendistro_security']['tools_dir'] = "#{node['elastic']['opendistro_security']['base_dir']}/tools"
-default['elastic']['opendistro_security']['tools']['hash'] = "#{node['elastic']['opendistro_security']['tools_dir']}/hash.sh"
-default['elastic']['opendistro_security']['tools']['securityadmin'] = "#{node['elastic']['opendistro_security']['tools_dir']}/securityadmin.sh"
+default['elastic']['opendistro']['version']                                         = "1.2.0.0"
+default['elastic']['opendistro_security']['enabled']                                = "true"
+default['elastic']['opendistro_security']['url']                                    = "#{node['download_url']}/opendistro_security-#{node['elastic']['opendistro']['version']}.zip"
+default['elastic']['opendistro_security']['base_dir']                               = "#{node['elastic']['plugins_dir']}/opendistro_security"
+default['elastic']['opendistro_security']['config_dir']                             = "#{node['elastic']['opendistro_security']['base_dir']}/securityconfig"
+default['elastic']['opendistro_security']['tools_dir']                              = "#{node['elastic']['opendistro_security']['base_dir']}/tools"
+default['elastic']['opendistro_security']['tools']['hash']                          = "#{node['elastic']['opendistro_security']['tools_dir']}/hash.sh"
+default['elastic']['opendistro_security']['tools']['securityadmin']                 = "#{node['elastic']['opendistro_security']['tools_dir']}/securityadmin.sh"
 
-default['elastic']['opendistro_security']['admin']['username'] = "admin"
-default['elastic']['opendistro_security']['admin']['password'] = "adminpw"
-default['elastic']['opendistro_security']['kibana']['username'] = "kibanaserver"
-default['elastic']['opendistro_security']['kibana']['password'] = "kibanaserver"
-default['elastic']['opendistro_security']['logstash']['username'] = "logstash"
-default['elastic']['opendistro_security']['logstash']['password'] = "logstash"
-default['elastic']['opendistro_security']['elastic_exporter']['username'] = "elastic_exporter"
-default['elastic']['opendistro_security']['elastic_exporter']['password'] = "elastic_exporter"
-default['elastic']['opendistro_security']['epipe']['username'] = "epipeuser"
-default['elastic']['opendistro_security']['epipe']['password'] = "epipepassword"
+default['elastic']['opendistro_security']['admin']['username']                      = "admin"
+default['elastic']['opendistro_security']['admin']['password']                      = "adminpw"
+default['elastic']['opendistro_security']['kibana']['username']                     = "kibanaserver"
+default['elastic']['opendistro_security']['kibana']['password']                     = "kibanaserver"
+default['elastic']['opendistro_security']['logstash']['username']                   = "logstash"
+default['elastic']['opendistro_security']['logstash']['password']                   = "logstash"
+default['elastic']['opendistro_security']['elastic_exporter']['username']           = "elastic_exporter"
+default['elastic']['opendistro_security']['elastic_exporter']['password']           = "elastic_exporter"
+default['elastic']['opendistro_security']['epipe']['username']                      = "epipeuser"
+default['elastic']['opendistro_security']['epipe']['password']                      = "epipepassword"
 
-default['elastic']['opendistro_security']['keystore']['type'] = "JKS"
-default['elastic']['kagent']['keystore']['location'] = node['install']['localhost'].casecmp?("true") ? "#{node['kagent']['keystore_dir']}/localhost__kstore.jks" : "#{node['kagent']['keystore_dir']}/#{node['fqdn']}__kstore.jks"
-default['elastic']['opendistro_security']['keystore']['file'] = "kstore.jks"
-default['elastic']['opendistro_security']['keystore']['location'] = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['keystore']['file']}"
-default['elastic']['opendistro_security']['keystore']['password'] = node['hopsworks']['master']['password']
+default['elastic']['opendistro_security']['keystore']['type']                       = "JKS"
+default['elastic']['kagent']['keystore']['location']                                = node['install']['localhost'].casecmp?("true") ? "#{node['kagent']['keystore_dir']}/localhost__kstore.jks" : "#{node['kagent']['keystore_dir']}/#{node['fqdn']}__kstore.jks"
+default['elastic']['opendistro_security']['keystore']['file']                       = "kstore.jks"
+default['elastic']['opendistro_security']['keystore']['location']                   = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['keystore']['file']}"
+default['elastic']['opendistro_security']['keystore']['password']                   = node['hopsworks']['master']['password']
 
-default['elastic']['opendistro_security']['truststore']['type'] = "JKS"
-default['elastic']['kagent']['truststore']['location'] = node['install']['localhost'].casecmp?("true") ?  "#{node['kagent']['keystore_dir']}/localhost__tstore.jks" : "#{node['kagent']['keystore_dir']}/#{node['fqdn']}__tstore.jks"
-default['elastic']['opendistro_security']['truststore']['file'] = "tstore.jks"
-default['elastic']['opendistro_security']['truststore']['location'] = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['truststore']['file']}"
-default['elastic']['opendistro_security']['truststore']['password'] = node['hopsworks']['master']['password']
+default['elastic']['opendistro_security']['truststore']['type']                     = "JKS"
+default['elastic']['kagent']['truststore']['location']                              = node['install']['localhost'].casecmp?("true") ?  "#{node['kagent']['keystore_dir']}/localhost__tstore.jks" : "#{node['kagent']['keystore_dir']}/#{node['fqdn']}__tstore.jks"
+default['elastic']['opendistro_security']['truststore']['file']                     = "tstore.jks"
+default['elastic']['opendistro_security']['truststore']['location']                 = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['truststore']['file']}"
+default['elastic']['opendistro_security']['truststore']['password']                 = node['hopsworks']['master']['password']
 
-default['elastic']['opendistro_security']['https']['enabled'] = "true"
-default['elastic']['opendistro_security']['ca'] = node["kagent"]["certs"]["root_ca"]
-default['elastic']['opendistro_security']['kibana']['multitenancy']['enabled'] = "true"
-default['elastic']['opendistro_security']['kibana']['index'] = ".kibana"
+default['elastic']['opendistro_security']['https']['enabled']                       = "true"
+default['elastic']['opendistro_security']['ca']                                     = node["kagent"]["certs"]["root_ca"]
+default['elastic']['opendistro_security']['kibana']['multitenancy']['enabled']      = "true"
+default['elastic']['opendistro_security']['kibana']['index']                        = ".kibana"
 
-default['elastic']['opendistro_security']['jwt']['enabled'] = "true"
-default['elastic']['opendistro_security']['jwt']['url_parameter'] = "jt"
-default['elastic']['opendistro_security']['jwt']['roles_key'] = "roles"
-default['elastic']['opendistro_security']['jwt']['subject_key'] = "sub"
-default['elastic']['opendistro_security']['jwt']['exp_ms'] = "600000"
+default['elastic']['opendistro_security']['jwt']['enabled']                         = "true"
+default['elastic']['opendistro_security']['jwt']['url_parameter']                   = "jt"
+default['elastic']['opendistro_security']['jwt']['roles_key']                       = "roles"
+default['elastic']['opendistro_security']['jwt']['subject_key']                     = "sub"
+default['elastic']['opendistro_security']['jwt']['exp_ms']                          = "600000"
 
-default['elastic']['opendistro_security']['roles']['data_owner']['role_name'] = 'data_owner'
-default['elastic']['opendistro_security']['roles']['data_scientist']['role_name'] = 'data_scientist'
+default['elastic']['opendistro_security']['roles']['data_owner']['role_name']       = 'data_owner'
+default['elastic']['opendistro_security']['roles']['data_scientist']['role_name']   = 'data_scientist'
 
-default['elastic']['opendistro_security']['audit']['enable_rest'] = "true"
-default['elastic']['opendistro_security']['audit']['enable_transport'] = "false"
-default['elastic']['opendistro_security']['audit']['type'] = "internal_elasticsearch"
-default['elastic']['opendistro_security']['audit']['threadpool']['size'] = 10
-default['elastic']['opendistro_security']['audit']['threadpool']['max_queue_len'] = 100000
+default['elastic']['opendistro_security']['audit']['enable_rest']                   = "true"
+default['elastic']['opendistro_security']['audit']['enable_transport']              = "false"
+default['elastic']['opendistro_security']['audit']['type']                          = "internal_elasticsearch"
+default['elastic']['opendistro_security']['audit']['threadpool']['size']            = 10
+default['elastic']['opendistro_security']['audit']['threadpool']['max_queue_len']   = 100000
 
 
-default['elastic']['epipe']['search_index'] = "projects"
-default['elastic']['epipe']['app_provenance_index'] = "appprovenance"
-default['elastic']['epipe']['file_provenance_index_pattern'] = "*__file_prov"
+default['elastic']['epipe']['search_index']                                         = "projects"
+default['elastic']['epipe']['app_provenance_index']                                 = "appprovenance"
+default['elastic']['epipe']['file_provenance_index_pattern']                        = "*__file_prov"
